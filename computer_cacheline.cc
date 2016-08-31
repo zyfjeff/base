a2074ad8b5a76a778ccf44dc960084007a4a9d73
天千 <tianqian.zyf@alibaba-inc.com>
2016-08-31 05:31:01 -0400

    computer cache line size

---
+++ computer_cacheline.cc
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <iostream>
+
+size_t cache_line_size() {
+    FILE * p = 0;
+    p = fopen("/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size", "r");
+    unsigned int i = 0;
+    if (p) {
+        fscanf(p, "%d", &i);
+        fclose(p);
+    }
+    return i;
+}
+
+
+int main()
+{
+    std::cout << cache_line_size() << std::endl;
+}
