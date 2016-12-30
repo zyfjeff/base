0c35f52108800eeb1175eadbba62a57190ade59e
tianqain.zyf <tianqian.zyf@alibaba-inc.com>
2016-12-30 11:22:09 +0800

    [添加] MurmurHash3 hash算法，并提供了逆操作，可以根据hash值反向计算其key值

---
+++ hash.h
@@ -0,0 +1,44 @@
+#ifndef _CODEBASE_HASH_H_
+#define _CODEBASE_HASH_H_
+#include <stdint.h>
+
+// from code.google.com/p/smhasher/wiki/MurmurHash3
+inline uint32_t avalanche(uint32_t h) {
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    return h;
+}
+
+inline uint32_t deavalanche(uint32_t h) {
+    h ^= h >> 16;
+    h *= 0x7ed1b41d;
+    h ^= (h ^ (h >> 13)) >> 13;
+    h *= 0xa5cb9243;
+    h ^= h >> 16;
+    return h;
+}
+
+// from code.google.com/p/smhasher/wiki/MurmurHash3
+inline uint64_t avalanche(uint64_t h) {
+    h ^= h >> 33;
+    h *= 0xff51afd7ed558ccd;
+    h ^= h >> 33;
+    h *= 0xc4ceb9fe1a85ec53;
+    h ^= h >> 33;
+    return h;
+}
+
+inline uint64_t deavalanche(uint64_t h) {
+    h ^= h >> 33;
+    h *= 0x9cb4b2f8129337db;
+    h ^= h >> 33;
+    h *= 0x4f74430c22a54005;
+    h ^= h >> 33;
+    return h;
+}
+
+
+#endif // end of _CODEBASE_HASH_H_
